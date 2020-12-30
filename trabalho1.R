3ede98f5b8726a932cd07fa138ba0a56adb89253
Carlos Alberto Torres Quintanilha Neto - DATAPREVRJ <carlos.quintanilha@RJOD268018.GOV>
2020-12-30 18:21:00 -0300

    dataset competidores ajustado

---
+++ trabalho1.R
@@ -0,0 +1,200 @@
+# Tratamento do comp_prices.csv
+install.packages("tidyverse")
+install.packages("ggpubr")
+install.packages("varhandle")
+install.packages("imputeTS")
+
+library(readr)
+library(dplyr)
+library(tidyverse)
+library(tibble)
+library(varhandle)
+library(imputeTS)
+
+
+theme_set(theme_linedraw())
+
+# Lendo os DataSets
+comp_prices <- read_csv("datasets/comp_prices.csv")
+
+sales <- read_csv("datasets/sales.csv")
+
+
+
+
+# Agrupando e Removendo valores duplicados
+
+comp_prices_grouped <- comp_prices %>% group_by(PROD_ID, DATE_EXTRACTION,COMPETITOR,PAY_TYPE) 
+comp_prices_grouped_distinct <- comp_prices_grouped %>% distinct()
+
+head(comp_prices_grouped_distinct)
+
+# Dando dimensão para a coluna e PAY_TYPE
+
+comp_prices_grouped_distinct <- comp_prices_grouped_distinct %>%
+  mutate(PAY_TYPE = as.factor(PAY_TYPE))
+    
+# Plotando os valores
+
+comp_prices_grouped_distinct %>% ggplot(aes(x = PAY_TYPE, y = COMPETITOR_PRICE, fill = COMPETITOR))+
+  geom_boxplot()+  
+  facet_wrap(~PROD_ID)+
+  xlab("Tipo de Pagamento") +
+  labs(fill="Competidores") 
+
+# Nota-se alguns outliers muito fora da média o que pode ser creditado a equivocos 
+# de digitação ou coleta. A visualização fica muito prejudicada.
+# Plotando os valores em escala retirando da visualização os outliers 
+# mais fora do normal.
+
+comp_prices_grouped_distinct %>% ggplot(aes(x = PAY_TYPE,y = COMPETITOR_PRICE, fill = COMPETITOR))+
+  geom_boxplot()+  
+  facet_wrap(~PROD_ID)+
+  xlab("Tipo de Pagamento") +
+  labs(fill="Competidores") +
+  scale_y_continuous(limits = c(0, 2000))
+
+# Visualmente ainda é possível verificar alguns outliers. 
+# Removendo Outliers de forma a deixar apenas valores entre 
+# [Q1- (1.5)IQR] e  [Q3+(1.5)IQR] e plotando 
+
+# Criando os limites inferior e superior
+
+Q1 <- quantile(comp_prices_grouped_distinct$COMPETITOR_PRICE, .25)
+
+Q3 <- quantile(comp_prices_grouped_distinct$COMPETITOR_PRICE, .75)
+
+IQRPRICE <- IQR(comp_prices_grouped_distinct$COMPETITOR_PRICE)
+
+# Criando o Dataset sem outliers pelo critério proposto
+comp_prices_ajusted <- subset(comp_prices_grouped_distinct, comp_prices_grouped_distinct$COMPETITOR_PRICE > (Q1 - 1.5*IQRPRICE) & comp_prices_grouped_distinct$COMPETITOR_PRICE < (Q3 + 1.5*IQRPRICE))
+head(comp_prices_ajusted)
+
+# Plotando os valores
+comp_prices_ajusted %>% ggplot(aes(x = PAY_TYPE,y = COMPETITOR_PRICE, fill = COMPETITOR))+
+  geom_boxplot()+  
+  facet_wrap(~PROD_ID)+
+  xlab("Tipo de Pagamento") +
+  labs(fill="Competidores") 
+
+# Preparando o DataSet por protudo
+comp_prices_nested <- comp_prices_ajusted %>% group_by(PROD_ID) %>% nest()
+
+# Testando por produto para verificar se o tipo de venda faz diferença nas medianas dos preços
+comp_prices_type_nested <- comp_prices_nested %>% mutate (pvalor = map(.x= data, .f = ~kruskal.test(.x$COMPETITOR_PRICE,.x$PAY_TYPE)$p.value))
+comp_prices_type_ploted <- comp_prices_type_nested %>% unnest(pvalor) %>% 
+  select (PROD_ID,pvalor)  
+comp_prices_type_ploted[comp_prices_type_ploted$pvalor > 0.05,]
+
+# Apenas o P4 confirma a hipótese nula de que as medianas são iguais, o que 
+# significa que, para a maioria dos produtos o tipo de venda faz diferença
+# para as medianas. 
+
+# Testando por produto para verificar se a medianas dos preços e diferente por concorrente.
+
+comp_prices_competitor_nested <- comp_prices_nested %>% mutate (pvalor = map(.x= data, .f = ~kruskal.test(.x$COMPETITOR_PRICE,.x$COMPETITOR)$p.value))
+comp_prices_competitor_ploted <- comp_prices_competitor_nested  %>% unnest(data) %>% unnest(pvalor) %>%
+  select (PROD_ID,pvalor)  
+comp_prices_competitor_ploted[comp_prices_competitor_ploted$pvalor > 0.05,]
+
+# Nenhum produto confirma a hipótese nula de que as medianas são iguais 
+# confirmando que as medianas são diferentes por concorrente para cada um 
+# dos produtos.
+
+
+# Retirando o horário da Data das medições
+comp_prices_ajusted$DATE_EXTRACTION <- as.Date(comp_prices_ajusted$DATE_EXTRACTION)
+
+# Agrupando por data de extração e sumarisando pela média diária por produto por concorrente. 
+
+comp_prices_ajusted <- comp_prices_ajusted %>% group_by(PROD_ID,DATE_EXTRACTION,COMPETITOR,PAY_TYPE) %>% summarise(COMPETITOR_PRICE = mean(COMPETITOR_PRICE))
+
+head(comp_prices_ajusted)
+
+# Pivotando o DataSet para trabalhar. 
+
+comp_pivoted <- comp_prices_ajusted %>% 
+                pivot_wider(names_from = c(COMPETITOR,PAY_TYPE), values_from = c(COMPETITOR_PRICE)) 
+
+head(comp_pivoted)
+
+# Adicionando lag de 1 dia, uma vez que a previsão será feita no dia anterior 
+
+comp_pivoted <- comp_pivoted %>% 
+  group_by(PROD_ID) %>% 
+  nest() %>% 
+  mutate(data = map(data, ~mutate_at(.x, paste0("C1_", 1:2), lag) )) %>%
+  mutate(data = map(data, ~mutate_at(.x, paste0("C2_", 1:2), lag) )) %>% 
+  mutate(data = map(data, ~mutate_at(.x, paste0("C3_", 1:2), lag) )) %>% 
+  mutate(data = map(data, ~mutate_at(.x, paste0("C4_", 1:2), lag) )) %>% 
+  mutate(data = map(data, ~mutate_at(.x, paste0("C5_", 1:2), lag) )) %>% 
+  mutate(data = map(data, ~mutate_at(.x, paste0("C6_", 1:2), lag) )) %>% 
+  unnest(cols = c(data)) %>% 
+  ungroup()
+
+head(comp_pivoted)
+
+# Quando o competidor não vende um determinado produto em um determinado tipo de pagamento toda a coluna passa 
+# a valer 0
+
+comp_pivoted <- comp_pivoted %>% nest(data = c(DATE_EXTRACTION, C1_1,C1_2, C2_1,C2_2, C3_1,C3_2,C4_1,C4_2, C5_1, C5_2, C6_1, C6_2)) %>% 
+  mutate(data = map(data, ~{
+    .x %>% 
+      # Caso toda coluna seja toda NA, input 0
+      mutate_if(~all(is.na(.x)), ~0)
+        
+  })) %>% 
+  unnest(cols = c(data))
+
+head(comp_pivoted)
+
+# Preenchendo os NA´s com a média
+
+  comp_pivoted <- comp_pivoted %>% 
+    #group_by(PROD_ID) %>% 
+    nest() %>% 
+    mutate(data = map(data, ~mutate_at(.x, paste0("C1_", 1:2), ~ifelse(is.na(.x),mean(.x,na.rm=T), .x)))) %>%
+    mutate(data = map(data, ~mutate_at(.x, paste0("C2_", 1:2), ~ifelse(is.na(.x),mean(.x,na.rm=T), .x)))) %>% 
+    mutate(data = map(data, ~mutate_at(.x, paste0("C3_", 1:2), ~ifelse(is.na(.x),mean(.x,na.rm=T), .x)))) %>% 
+    mutate(data = map(data, ~mutate_at(.x, paste0("C4_", 1:2), ~ifelse(is.na(.x),mean(.x,na.rm=T), .x)))) %>% 
+    mutate(data = map(data, ~mutate_at(.x, paste0("C5_", 1:2), ~ifelse(is.na(.x),mean(.x,na.rm=T), .x)))) %>% 
+    mutate(data = map(data, ~mutate_at(.x, paste0("C6_", 1:2), ~ifelse(is.na(.x),mean(.x,na.rm=T), .x)))) %>% 
+    unnest(cols = c(data))
+      
+
+head(comp_pivoted)
+
+# Criando a coluna preço unitário
+
+sales <- sales %>% mutate(P_Unitario = REVENUE/QTY_ORDER)
+
+head(sales)
+
+# Plotando os valores
+sales %>% ggplot(aes(x = PROD_ID, y = P_Unitario, fill=PROD_ID ))+
+  geom_boxplot()
+
+# É possível notar alguns outliers, utilizaremos o mesmo critério para removê-los, 
+# isto é [Q1- (1.5)IQR] e  [Q3+(1.5)IQR] 
+
+# Criando os limites inferior e superior
+
+Q1 <- quantile(sales$P_Unitario, .25)
+
+Q3 <- quantile(sales$P_Unitario, .75)
+
+IQRPRICE <- IQR(sales$P_Unitario)
+
+# Criando o Dataset sem outliers pelo critério proposto e plotando
+sales <- subset(sales, sales$P_Unitario > (Q1 - 1.5*IQRPRICE) & sales$P_Unitario < (Q3 + 1.5*IQRPRICE))
+
+sales %>% ggplot(aes(x = PROD_ID, y = P_Unitario, fill=PROD_ID ))+
+  geom_boxplot()
+
+sales <- sales %>% 
+  group_by(PROD_ID, DATE_ORDER) %>% 
+  summarise(QTY_ORDER = sum(QTY_ORDER),
+            REVENUE = sum(REVENUE))
+  
+sales <- sales %>% mutate(P_Unitario = REVENUE/QTY_ORDER)
+head(sales)
